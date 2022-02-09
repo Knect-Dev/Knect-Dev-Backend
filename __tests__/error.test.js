e225dd2d09282a7c4d7073176d60c6b0f63f6f3d
erik savage <erik.k.savage@gmail.com>
2022-02-09 10:56:50 -0800

    outlines error handler tests

---
+++ __tests__/error.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const err403 = require('../lib/error-handlers/403');
+const err404 = require('../lib/error-handlers/404');
+const err500 = require('../lib/error-handlers/500');
+
+describe('403 Error Handler Tests', () => {
+  it('should respond with a 403 status and proper error object', () => {
+
+  });
+});
+
+describe('404 Error Handler Tests', () => {
+  it('should repond with a 404 status and proper error object', () => {
+
+  });
+});
+
+describe('500 Error Handler Tests', () => {
+  it('should repond with a 500 error status and proper error object', () => {
+
+  });
+});
