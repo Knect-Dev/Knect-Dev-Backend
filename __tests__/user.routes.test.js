7d6bb383fda19e06874e9b5f564b77c7258a80b5
erik savage <erik.k.savage@gmail.com>
2022-02-09 14:38:42 -0800

    adds user route test outline

---
+++ __tests__/user.routes.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { server } = require('../lib/server');
+const supertest = require('supertest');
+const request = supertest(server);
+
+describe('User Route Tests', () => {
+  it('should respond with a single user when accessed by admin ', () => {
+    // a get request (with id) should result in a single user
+  });
+  it('should respond with a list of users when accessed by admin ', () => {
+    // a get request (no id) should result in a list of all users
+  });
+
+});
