c3b28fc422de0ee1fa20c4ca4c3b82d74097a764
jamiguel23 <josematthewmiguel@gmail.com>
2022-02-10 17:06:31 -0800

    route testing pass

--- __tests__/logger.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const morgan = require('morgan');
-
-describe('Logger Middleware Tests', () => {
-  // it('should respond with the request method and request path', () => {
-
-  // });
-});
--- __tests__/user.routes.test.js
+++ __tests__/user.routes.test.js
@@ -4,12 +4,104 @@ const { server } = require('../lib/server');
 const supertest = require('supertest');
 const request = supertest(server);
 
-describe('User Route Tests', () => {
-  // it('should respond with a single user when accessed by admin ', () => {
-  //   // a get request (with id) should result in a single user
-  // });
-  // it('should respond with a list of users when accessed by admin ', () => {
-  //   // a get request (no id) should result in a list of all users
-  // });
+const testUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RVc2VyQHRlc3QuY29tIiwiaWF0IjoxNjQ0NTM4NDE1fQ.ixFkzaqQDwyR1We3dEUl4ODd9vmSZRjtSf57VbddfXI';
 
+const testAdminToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RBZG1pbkB0ZXN0LmNvbSIsImlhdCI6MTY0NDUzODQ1MH0.r_VJjXbatu98qmtVvYJih1K1xJq4OSOEp25qj36Fp8U';
+
+const testUserId = '8DwN0nPhiIgA3MwaRY-U5';
+
+const testUser = {
+  firstName: 'goliath',
+  lastName: 'miguel-reyes',
+  password: 'test',
+  email: 'goliath@doge.com'
+
+}
+
+describe('User Route Tests', () => {
+
+  it('should not be allowed to create a user when using POST ', async () => {
+    // create one company record
+    const userResponse = await request.post('/Users').set('Authorization', `Bearer ${testUserToken}`).send(testUser);
+    expect(userResponse.status).toEqual(500);
+    
+    const adminResponse = await request.post('/Users').set('Authorization', `Bearer ${testAdminToken}`).send(testUser);
+    expect(adminResponse.status).toEqual(500);
+    // user and admin should be able to do this
+  });
+
+  it('should read a record when using GET and an Id ', async () => {
+    
+    //user-findOne
+    const response = await request.get(`/Users/${testUserId}`).set('Authorization', `Bearer ${testUserToken}`);
+    expect(response.status).toEqual(200);
+    expect(response.body).toBeDefined();
+    expect(response.body[0].firstName).toEqual('testUser');
+    
+    //admin-findOne
+    const adminResponse = await request.get(`/Users/${testUserId}`).set('Authorization', `Bearer ${testAdminToken}`);
+    expect(adminResponse.status).toEqual(200);
+    expect(adminResponse.body).toBeDefined();
+    expect(adminResponse.body[0].firstName).toEqual('testUser');
+  });
+
+  // return a list of all companies
+  it('should read all records when using GET no other search params ', async () => {
+    //User-findAll
+    const response = await request.get('/Users').set('Authorization', `Bearer ${testUserToken}`);
+    expect(response.status).toEqual(500);
+    // admin-findAll
+    const adminResponse = await request.get('/Users').set('Authorization', `Bearer ${testAdminToken}`);
+    expect(adminResponse.status).toEqual(200);
+    expect(adminResponse.body).toBeDefined();
+    expect(adminResponse.body.length > 1).toBe(true); // Assumes more than one entry in our company DB
+  });
+
+  
+  // update company record
+  it('should update a record when using PUT and an Id ', async () => {
+    //user-put
+    const response = await request.put(`/Users/${testUserId}`).set('Authorization', `Bearer ${testUserToken}`).send({
+      email: 'testemail@doge.com',
+    });
+    expect(response.status).toEqual(202);
+    expect(response.body.email).toEqual('testemail@doge.com');
+    // admin-put
+    const adminResponse = await request.put(`/Users/${testUserId}`).set('Authorization', `Bearer ${testAdminToken}`).send({
+      email: 'testUser@test.com',
+    });
+    expect(adminResponse.status).toEqual(202);
+    expect(adminResponse.body.email).toEqual('testUser@test.com');
+  });
+
+
+  //delete a record
+  let userCreatedResponse;
+
+  it('User should not be able to delete another user', async () => {
+    userCreatedResponse = await request.post('/signup').send(testUser);
+  //user-delete
+
+    console.log(userCreatedResponse.body.user.id)
+
+    const response = await request.delete(`/Users/${userCreatedResponse.body.user.id}`).set('Authorization', `Bearer ${testUserToken}`);
+    expect(response.status).toEqual(500);
+  });
+
+    //delete a record
+  it('User should be able to delete themselves', async () => {
+    //user-delete
+      const response = await request.delete(`/Users/${userCreatedResponse.body.user.id}`).set('Authorization', `Bearer ${userCreatedResponse.body.token}`);
+      expect(response.status).toEqual(200);
+  });
+
+  it('Admin should be able to delete a user when using DELETE and an Id', async () => {
+
+    const adminUserCreatedResponse = await request.post('/signup').send(testUser);
+
+    console.log(adminUserCreatedResponse.body.user.id)
+    //admin-delete
+    const response = await request.delete(`/Users/${adminUserCreatedResponse.body.user.id}`).set('Authorization', `Bearer ${testAdminToken}`);
+    expect(response.status).toEqual(200);
+  });
 });
