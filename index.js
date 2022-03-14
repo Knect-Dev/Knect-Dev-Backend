4e01bde6ba775103d30ea7296ccf30ff8ffcffde
erik savage <erik.k.savage@gmail.com>
2022-03-14 13:19:01 -0700

    tweaks to get server started with created tables

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const { start } = require('./lib/server');
+const { db } = require('./lib/models');
 
-start(PORT);
+db.sync()
+  .then(() => start(PORT))
+  .catch((error) => console.log(error));
